b588e77712f96c2e637b6ddeafb48e6f793e0732
Karatanov <karatanov@gmail.com>
2016-02-02 23:57:20 +0200

    class

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
 
-QPushButton button("Hello, World!");
-button.resize(200, 60);
-button.show( );
+QLabel label("<h2><i>Hello,</i> " "<font color = red > Qt! </font></h2>");
+label.show( );
 
 return app.exec( );
      }
