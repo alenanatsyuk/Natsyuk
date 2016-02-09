9bcce139f97803d3d9200a6f1663249087fe4ead
Karatanov <karatanov@gmail.com>
2016-02-09 21:35:58 +0200

    Semenov

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
 
-QLabel *label = new QLabel ("<h2><i>Hello,</i> " "<font color = red > Qt! </font> </h2>");
+QLabel *label = new QLabel ("<h2><i>Semenov is</i> " "<font color = red > the most beautiful! </font> </h2>");
 label->show( );
 
 return app.exec( );
-     }
+}
