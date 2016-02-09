737385e585b83f09d4115f255c8290d6232d8c4c
Karatanov <karatanov@gmail.com>
2016-02-09 21:33:48 +0200

    Makar

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
 
-QLabel label("<h2><i>Макаренко самый</i> " "<font color = red > умный! </font></h2>");
+QLabel label("<h2><i>Makarenko is</i> " "<font color = red > the best! </font></h2>");
 label.show( );
 
 return app.exec( );
-     }
+}
